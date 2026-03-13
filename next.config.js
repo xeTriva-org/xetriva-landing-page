84e48428cfe8b1361ec4cff7978600c028838184
swapnilahmedshishir <ahmed66882298@gmail.com>
2026-03-13 16:06:13 +0600

    create the xetriva app

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
