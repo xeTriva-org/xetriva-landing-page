95c4200b2e3c753279d51d2c8812586ade02fa23
swapnilahmedshishir <ahmed66882298@gmail.com>
2026-03-13 19:22:29 +0600

    refactor: simplify next.config.js and add xetriva page outline

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
